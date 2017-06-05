bfbcdeabd8edc6b80c248a20c3ed978eea799ef5
Andrei Yechkalov <echkalov@ukr.net>
2017-06-05 23:47:37 +0300

    Initial commit

---
+++ app/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Corp;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+/**
+ * Corp\User
+ *
+ * @mixin \Eloquent
+ */
+class User extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
---
+++ database/migrations/2017_06_05_191918_create_table_articles.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableArticles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',255);
+            $table->text('text');
+            $table->text('descript');
+            $table->string('alias',150)->unique();
+            $table->string('image',150);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
