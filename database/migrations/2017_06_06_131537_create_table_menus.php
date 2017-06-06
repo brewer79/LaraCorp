0863f41c4419ff7a79b09bda20ed35bdf79594fa
Andrei Yechkalov <echkalov@ukr.net>
2017-06-06 17:29:02 +0300

    up

---
+++ database/migrations/2017_06_06_131537_create_table_menus.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableMenus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('path');
+            $table->integer('parent_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('menus');
+    }
+}
