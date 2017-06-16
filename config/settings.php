acd3d3b849bf99112597b6e385cadf657250353d
Andrei Yechkalov <echkalov@ukr.net>
2017-06-16 17:23:38 +0300

    up

--- config/settings.php
+++ config/settings.php
@@ -4,7 +4,7 @@ return [
     'slider_path' => 'slider-cycle',
     'homepage_portfolio_count' => 5,
     'homepage_articles_count' => 3,
-    'paginate' => 2,
+    'paginate' => 5,
     'recent_comments' => 3,
     'recent_portfolios' => 3
 
